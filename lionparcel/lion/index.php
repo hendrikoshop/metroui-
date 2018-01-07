14f1b0d097f8bfe5bf8057615412557669e7dd70
hendrikoshop <hendrikoshop@github.com>
2018-01-07 23:25:14 +0700

    metro ui
    tugas 3 desain web praktek

---
+++ lionparcel/lion/index.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Flat desain :LION PARCEL</title>
+        <link rel="stylesheet" type="text/css" href="assets/css/onepage-scroll.css" />
+        <link href='assets/fonts/Righteous.css' rel='stylesheet' type='text/css'>
+        <link href='assets/fonts/Voltaire.css' rel='stylesheet' type="text/css">
+        <link rel="stylesheet" href="assets/css/style.css">
+    </head> 
+    <body>
+        <header>
+            <img src="assets/img/lion.JPG" width="150" height="110px" alt="logo mini">
+            <h1><a href="#" style="color:#e32025">LION PARCEL</a></h1>
+        </header>
+        <header>
+         	<nav>
+        		<ul>
+            		<a href="#"><li>PROFILE</li></a>
+                    <a href="#"><li>NEWS</li></a>
+               		<a href="#"><li>TRACKING</li></a>
+               	 	<a href="#"><li>CONTACT</li></a>
+                	<a href="#"><li>INFORMATION</li></a>
+            	</ul>
+        	</nav>
+        </header>
+        <div class="main">
+            <section class="page one">
+                <div class="page-container">
+                    <h2 style="color:#e32025">PRODUCT ONE PACK Dan REGPACK LION PARCEL</h2>
+                    <p align="justify"> PT. Lion Parcel sebagai salah satu bisnis unit usaha Lion Group di bidang jasa pengiriman barang yang memiliki modal jaringan dan operasional yang terpercaya dengan dukungan armada pesawat dan frekuensi penerbangan yang tinggi sebagai linehaul ke hampir seluruh bandar udara di Indonesia. Product unggulan dari lion parcel ada ONE PACK SERVICE dan REGPACK.   Lion Parcel memberikan pelayanan pengiriman barang dan dokumen yang terintegrasi dan kepastian dengan jaringan outlet yang tersebar hingga ke daerah – daerah pelosok nusantara.
+                    Lion Parcel terus mengembangkan jaringan dan kinerja pelayanannya untuk memberikan kepastian dan keamanan pengiriman barang dan dokumen yang didukung oleh sistem informasi dan operasi yang terintegrasi serta komitmen atas performa pelayanan atas prioritas operasi guna meningkatkan kualitas pelayanan secara berkesinambungan. Didukung oleh sistem operasi yang berbasis internet (web based), pengiriman dapat dilacak dengan secara online melalui sistem sehingga memastikan barang dan dokumen yang dikirim diterima tepat waktu.<br>Source : https://www.youtube.com/watch?v=2qCtVz1kz3s</p>
+                    <video width="400" controls>
+                    <source src="assets/vid/lion.mp4" align="center" type="video/mp4"/> 
+                    
+                </div>
+            </section>
+            
+            <section class="page two">
+                <div class="page-container">
+                    <h2 style="color:#e32025">VISI Dan MISI</h2>
+                    <h3>Visi</h3>
+                    <p align="justify">Menjadi perusahaan penyedia jasa pengiriman, transportasi dan distribusi terkemuka dalam lingkup nasional dan international.</p>                         
+                    <h4>Misi</h4>
+                    <p>Berperan penting dalam mempercepat pertumbuhan ekonomi nasional dengan menyediakan jasa pengiriman, transportasi dan distribusi yang tercepat dan terpercaya untuk memenuhi kebutuhan dan kepuasan pelanggan</p>
+                </div>
+            </section>
+            
+            <section class="page three">
+                <div class="page-container">
+                    
+                    <div id="home-artikel-left">
+                        <div class="artikel"><img src="assets/img/onepack.png" width="200" height="200" alt="">
+                        </div>
+       
+                        <div class="artikel"><img src="assets/img/kurir.jpg" width="200" height="200" alt="">
+                        </div>
+                    </div>
+
+                    <div id="home-artikel-right">
+                        <div class="artikel"><img src="assets/img/regpack.png" width="200" height="200" alt="">
+                        </div>
+
+                        <div class="artikel"><img src="assets/img/lion3.png" width="200" height="200" alt=""></div>
+                    </div>
+                </div>
+            </section>
+        </div>
+        <script type="text/javascript" src="assets/js/jquery-1.9.1.js"></script>
+        <script type="text/javascript" src="assets/js/jquery.onepage-scroll.js"></script>
+        <script type="text/javascript">
+        $(".main").onepage_scroll({
+                sectionContainer: "section",
+                easting: "cubic-bezier(0.180, 0.900, 0.410, 1.210)"
+        });
+        </script>
+    </body>
+</html>
